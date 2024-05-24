10a22d4cbebd9a659ea84f9beb072b694ca0e18c
Joseph Scanlon <jscanlon.nyc@gmail.com>
2024-05-24 18:14:29 -0400

    update 2

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButtons = document.querySelectorAll(".toggle-section");
+
+    toggleButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            const content = this.parentElement.nextElementSibling;
+            content.classList.toggle("hidden");
+            this.textContent = content.classList.contains("hidden") ? "Expand" : "Collapse";
+        });
+    });
+});
+
+
+function downloadEbook() {
+    var link = document.createElement('a');
+    link.href = 'assets/joseph_scanlon_resume.epub';
+    link.download = 'joseph_scanlon_resume.epub'; 
+    link.click();
+}
